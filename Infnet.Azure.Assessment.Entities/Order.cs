bd9ca8994a8de66baa3bcd646ff15558087b8411
Munir Wanis <fallen.angel.pp@gmail.com>
2017-06-29 04:07:36 -0300

    Add entities :

---
+++ Infnet.Azure.Assessment.Entities/Order.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Infnet.Azure.Assessment.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public bool IsSent { get; set; }
+
+        public bool IsSaved { get; set; }
+
+        public List<Product> Products { get; set; }
+    }
+}
---
+++ Infnet.Azure.Assessment.Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿namespace Infnet.Azure.Assessment.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Category { get; set; }
+
+        public long Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        public virtual Order Order { get; set; }
+    }
+}
